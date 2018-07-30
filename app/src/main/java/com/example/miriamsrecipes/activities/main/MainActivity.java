ff34836aebf80f6c3c20353c2dbb0319bd52e9b9
DavidPrecopia <35779726+DavidPrecopia@users.noreply.github.com>
2018-07-29 21:05:42 -0400

    Replace MainActivity's ActionBar with the Toolbar.

--- app/src/main/java/com/example/miriamsrecipes/activities/main/MainActivity.java
+++ app/src/main/java/com/example/miriamsrecipes/activities/main/MainActivity.java
@@ -2,24 +2,25 @@ package com.example.miriamsrecipes.activities.main;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 import com.example.miriamsrecipes.R;
 import com.example.miriamsrecipes.activities.recipe.RecipeActivity;
+import com.example.miriamsrecipes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 	
 	private MainViewModel viewModel;
+	private ActivityMainBinding binding;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 		
-		Toolbar toolbar = findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
+		setSupportActionBar(binding.appBar.toolbar);
 		
 		ViewModelFactory factory = new ViewModelFactory(getApplication());
 		viewModel = ViewModelProviders.of(this, factory).get(MainViewModel.class);
