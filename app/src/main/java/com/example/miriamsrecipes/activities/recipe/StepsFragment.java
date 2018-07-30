b196cbd83ff3e6ee85019efe52329bba506751e2
DavidPrecopia <35779726+DavidPrecopia@users.noreply.github.com>
2018-07-30 15:12:49 -0400

    Add Fragment starter files.

---
+++ app/src/main/java/com/example/miriamsrecipes/activities/recipe/StepsFragment.java
@@ -0,0 +1,25 @@
+package com.example.miriamsrecipes.activities.recipe;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.miriamsrecipes.R;
+
+public class StepsFragment extends Fragment {
+	
+	
+	public StepsFragment() {
+	}
+	
+	
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_steps, container, false);
+	}
+	
+}
