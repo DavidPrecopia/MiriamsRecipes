efc21cb757356b20b9a5160b9cf4e6d07b58f1cf
DavidPrecopia <35779726+DavidPrecopia@users.noreply.github.com>
2018-07-30 23:38:14 -0400

    Write RecipeActivity.

--- app/src/main/java/com/example/miriamsrecipes/activities/recipe/RecipeActivity.java
+++ app/src/main/java/com/example/miriamsrecipes/activities/recipe/RecipeActivity.java
@@ -1,17 +1,34 @@
 package com.example.miriamsrecipes.activities.recipe;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.miriamsrecipes.R;
 
-public class RecipeActivity extends AppCompatActivity {
+public class RecipeActivity extends AppCompatActivity implements StepsFragment.FragmentClickListener {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_recipe);
+		DataBindingUtil.setContentView(this, R.layout.activity_recipe);
 		
-		
+		if (savedInstanceState == null) {
+			initializeFragment();
+		}
+	}
+	
+	private void initializeFragment() {
+		StepsFragment stepsFragment = StepsFragment.newInstance(
+				getIntent().getParcelableExtra(RecipeActivity.class.getSimpleName())
+		);
+		getSupportFragmentManager().beginTransaction()
+				.add(R.id.fragment_holder, stepsFragment)
+				.commit();
+	}
+	
+	@Override
+	public void onStepClick(int stepId) {
+		// TODO Implement onClick
 	}
 }
