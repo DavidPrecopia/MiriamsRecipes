d2b161f0e2ba01146590b30a08182565cfe59f66
DavidPrecopia <35779726+DavidPrecopia@users.noreply.github.com>
2018-08-08 18:37:57 -0400

    Have MainViewModel store the List from the Model - avoids re-querying the Model on rotation.

--- app/src/main/java/com/example/miriamsrecipes/activities/main/MainViewModel.java
+++ app/src/main/java/com/example/miriamsrecipes/activities/main/MainViewModel.java
@@ -3,6 +3,8 @@ package com.example.miriamsrecipes.activities.main;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
 import android.support.annotation.NonNull;
 
 import com.example.miriamsrecipes.model.IModelContract;
@@ -12,14 +14,32 @@ import java.util.List;
 
 final class MainViewModel extends AndroidViewModel {
 	
+	private MutableLiveData<List<RecipeInfo>> recipes;
+	private Observer<List<RecipeInfo>> observer;
+	
 	private final IModelContract model;
 	
 	MainViewModel(@NonNull Application application) {
 		super(application);
+		this.recipes = new MutableLiveData<>();
+		this.observer = (recipeInfoList -> this.recipes.setValue(recipeInfoList));
 		this.model = Model.getInstance(application);
+		observeModel();
 	}
 	
+	private void observeModel() {
+		model.getAllRecipes().observeForever(observer);
+	}
+	
+	
 	LiveData<List<RecipeInfo>> getRecipes() {
-		return model.getAllRecipes();
+		return recipes;
+	}
+	
+	
+	@Override
+	protected void onCleared() {
+		super.onCleared();
+		model.getAllRecipes().removeObserver(observer);
 	}
 }
