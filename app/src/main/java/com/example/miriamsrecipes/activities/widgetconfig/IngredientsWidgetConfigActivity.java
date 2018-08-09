25175ae84013fd6ae1663200646ec88ea1cf490a
DavidPrecopia <35779726+DavidPrecopia@users.noreply.github.com>
2018-08-08 20:17:54 -0400

    Add starter files for IngredientsWidgetConfigurationActivity.

---
+++ app/src/main/java/com/example/miriamsrecipes/activities/widgetconfig/IngredientsWidgetConfigActivity.java
@@ -0,0 +1,15 @@
+package com.example.miriamsrecipes.activities.widgetconfig;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.miriamsrecipes.R;
+
+public class IngredientsWidgetConfigActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ingredients_widget_configuration);
+	}
+}
