b34a2bc7954b0970adbb884e4fdbc77650f50072
DavidPrecopia <35779726+DavidPrecopia@users.noreply.github.com>
2018-08-11 00:52:06 -0400

    Write and pass test of SingleStepNavigation.

--- app/src/androidTest/java/com/example/miriamsrecipes/RecipeActivityTest.java
+++ app/src/androidTest/java/com/example/miriamsrecipes/RecipeActivityTest.java
@@ -14,24 +14,30 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.PositionAssertions.isCompletelyAbove;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
+public class RecipeActivityTest {
 	
 	@Rule
 	public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 	
+	
 	@Test
 	public void testIngredientsLoaded() {
-		IdlingRegistry idlingRegistry = IdlingRegistry.getInstance();
-		
-		idlingRegistry.register(mainActivityTestRule.getActivity().getCountingIdlingResource());
+		IdlingRegistry.getInstance().register(mainActivityTestRule.getActivity().getCountingIdlingResource());
 		onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-		
 		onView(withId(R.id.ingredient_list_item)).check(isCompletelyAbove(withId(R.id.recycler_view)));
 	}
 	
-	
-	
+	@Test
+	public void testStepNavigation() {
+		IdlingRegistry.getInstance().register(mainActivityTestRule.getActivity().getCountingIdlingResource());
+		onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+		onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+		onView(withId(R.id.iv_next_arrow)).perform(click());
+		onView(withId(R.id.tv_current_step)).check(matches(withText("2")));
+	}
 }
